cca27d205aa51970d6ab245dc0467452a7c0e0b1
rajnishcoder <getwere@gmail.com>
2018-04-06 14:34:04 +0530

    todays push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	mainUserInput: string;
+
+	ngOnInit(): void {
+	}
+
+	onInputEnter(mainUserInput: string) {
+		
+	}
+
 }
